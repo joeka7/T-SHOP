a8ef7443aae24f5c729a7d86ebc29a6fc1ca5ea1
Joe-ka <ykaram599@gmail.com>
2023-02-10 18:13:54 +0200

    Update

--- js/master.js
+++ js/master.js
@@ -18,6 +18,45 @@ colorsLi.forEach(li => {
         e.target.classList.add("active");
     });
 });
+let backgroundOption = true;
+let backgroundInterval;
+// Check If There's Local Storage Random Background Item
+let backgroundLocalItem = localStorage.getItem("background_option");
+if (backgroundLocalItem !== null) {
+    if (backgroundLocalItem === "true") {
+        backgroundOption = true;
+    } else {
+        backgroundOption = false;
+    };
+    // Remove Active Class From All Spans
+    document.querySelectorAll(".random-background span").forEach(e => {
+        e.classList.remove("active");
+    });
+    if (backgroundLocalItem === "true") {
+        document.querySelector(".random-background .yes").classList.add("active");
+    } else {
+        document.querySelector(".random-background .no").classList.add("active");
+    }
+};
+// Switch Random Background Option
+const randomBackgroundsEle = document.querySelectorAll(".random-background span")
+randomBackgroundsEle.forEach(li => {
+    li.addEventListener("click", (e) => {
+        e.target.parentElement.querySelectorAll(".active").forEach(element => {
+            element.classList.remove("active");
+        });
+        e.target.classList.add("active");
+        if (e.target.dataset.background === "yes") {
+            backgroundOption = true;
+            randomImgs();
+            localStorage.setItem("background_option", true);
+        } else {
+            backgroundOption = false;
+            clearInterval(backgroundInterval);
+            localStorage.setItem("background_option", false);
+        };
+    });
+});
 // Check If There's Local Storage Color Option
 let mainColors = localStorage.getItem("color-option");
 if (mainColors !== null) {
@@ -30,11 +69,17 @@ if (mainColors !== null) {
             element.classList.add("active");
         };
     });
-}
+};
 // Change Background Image
 let landingPage = document.querySelector(".landing-page");
 let imgsArray = ["1.jpg", "2.jpg", "3.jpg", "4.jpg"];
-setInterval(() => {
-    let randomNumber = Math.floor(Math.random() * imgsArray.length);
-    landingPage.style.backgroundImage = 'url("/imgs/T-SHOP_'+ imgsArray[randomNumber] +'")';
-}, 8000);
+// Random Background Option
+function randomImgs() {
+    if (backgroundOption === true) {
+        backgroundInterval = setInterval(() => {
+            let randomNumber = Math.floor(Math.random() * imgsArray.length);
+            landingPage.style.backgroundImage = 'url("/imgs/T-SHOP_'+ imgsArray[randomNumber] +'")';
+        }, 8000);
+    };
+};
+randomImgs();
