dea13bccc4ce0514ac5fddc1e61455f028ff621c
Joe-ka <ykaram599@gmail.com>
2023-02-12 23:30:08 +0200

    Update

--- js/master.js
+++ js/master.js
@@ -2,6 +2,7 @@
 let toggler = document.querySelector(".toggle");
 let nav = document.querySelector("nav");
 let done = document.querySelector(".close");
+let settingBox = document.querySelector(".setting-box");
 toggler.onclick = function() {
     nav.style.transform = "translateY(0)";
 };
@@ -26,17 +27,6 @@ arrowIcon.addEventListener("click", function() {
         behavior: "smooth",
     });
 });
-// arrowIcon.onclick = function() {
-//     window.scrollTo({
-//         top: 0,
-//         behavior: "smooth",
-//     });
-// };
-// Toggle Sping Class On Icon
-document.querySelector(".toggle-settings i").onclick = function() {
-    this.classList.toggle("fa-spin");
-    document.querySelector(".setting-box").classList.toggle("open");
-};
 // Switch Colors
 const colorsLi = document.querySelectorAll(".colors-list li");
 colorsLi.forEach(li => {
@@ -54,43 +44,6 @@ colorsLi.forEach(li => {
 });
 let backgroundOption = true;
 let backgroundInterval;
-// Check If There's Local Storage Random Background Item
-let backgroundLocalItem = localStorage.getItem("background_option");
-if (backgroundLocalItem !== null) {
-    if (backgroundLocalItem === "true") {
-        backgroundOption = true;
-    } else {
-        backgroundOption = false;
-    };
-    // Remove Active Class From All Spans
-    document.querySelectorAll(".random-background span").forEach(e => {
-        e.classList.remove("active");
-    });
-    if (backgroundLocalItem === "true") {
-        document.querySelector(".random-background .yes").classList.add("active");
-    } else {
-        document.querySelector(".random-background .no").classList.add("active");
-    }
-};
-// Switch Random Background Option
-const randomBackgroundsEle = document.querySelectorAll(".random-background span")
-randomBackgroundsEle.forEach(li => {
-    li.addEventListener("click", (e) => {
-        e.target.parentElement.querySelectorAll(".active").forEach(element => {
-            element.classList.remove("active");
-        });
-        e.target.classList.add("active");
-        if (e.target.dataset.background === "yes") {
-            backgroundOption = true;
-            randomImgs();
-            localStorage.setItem("background_option", true);
-        } else {
-            backgroundOption = false;
-            clearInterval(backgroundInterval);
-            localStorage.setItem("background_option", false);
-        };
-    });
-});
 // Check If There's Local Storage Color Option
 let mainColors = localStorage.getItem("color-option");
 if (mainColors !== null) {
