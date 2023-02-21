c2ef39c74543477993eabf958f7095714735b78d
Joe-ka <ykaram599@gmail.com>
2023-02-21 22:15:51 +0200

    Update

--- js/log.js
+++ js/log.js
@@ -2,9 +2,15 @@ let mediaLog = window.matchMedia("(max-width: 767px)");
 let emailInput = document.querySelector(".log-in-container form .email input");
 let submitBtn = document.querySelector(".log-in-container form .submit-btn");
 let socialMedia = document.querySelector(".log-in .log-in-container .log-in-social");
+let lineResponsive = document.querySelector(".log-in .log-in-container .line-responsive");
+let loginMobile = document.querySelector(".log-in .log-in-container .login-mobile");
+let logIn = document.querySelector(".log-in");
 emailInput.addEventListener("click", function() {
     if (mediaLog.matches) {
         submitBtn.style.cssText = "display: block"
         socialMedia.style.cssText = "display: none"
-    }
+        lineResponsive.style.cssText = "display: none";
+        loginMobile.style.cssText = "display: flex";
+        logIn.style.cssText = "height: 100vh";
+    };
 });
